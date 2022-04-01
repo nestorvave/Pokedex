1b361780309d1c421493874a4f5d3b4610e4ce49
nestorvave <84485292+nestorvave@users.noreply.github.com>
2022-03-31 18:39:11 -0600

    styles added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import './App.css'
 
 function App() {
+  const [poke, setPoke] = useState(null);
+
+  useEffect;
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <main className="container" >
+        <nav></nav>
+        <article></article>
+      </main>
+    </BrowserRouter>
   );
 }
 
