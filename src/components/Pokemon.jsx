ac42a6f17f610592909cd27618fe456318e47434
nestorvave <84485292+nestorvave@users.noreply.github.com>
2022-03-31 19:51:12 -0600

    pokemon displayed

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,19 +1,33 @@
 import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Pokemon({ name }) {
-
+    const[poke, setPoke]=useState({
+        image:""
+    })
     useEffect(() => {
         const request = async () => {
             const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
             const resp = await data.json();
-            console.log(resp);
+            const {  sprites } = resp
+            setPoke({
+                image:sprites.front_default
+            })
         };
         if (name) {
             request();
         }
     }, [])
+    const {  image } = poke
 
     return (
-        <div>Pokemon</div>
+        <section>
+            <Link to="/" >Volver</   Link>
+            <div>
+                <h1> {name} </h1>
+                <img src={image} alt={name}  />
+            </div>
+        </section>
     )
 }
